d2e0c7e49bdee4df828db36f7d8ce19981996f03
naseem <abbasmuhsin03@gmail.com>
2021-09-20 22:19:06 +0500

    constants

--- App.js
+++ App.js
@@ -2,10 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import HomeContainer from './assets/src/container/HomeContainer'
+import { Provider } from 'react-redux';
 export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
+      <Provider />
       <HomeContainer />
     </View>
   );
